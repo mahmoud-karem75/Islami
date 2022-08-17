3d2d24440daf4804ef2996139694b71f0f0439bf
HoOdA <mmahmoudkarem7@gmail.com>
2022-08-17 12:18:25 +0200

    starting the project

---
+++ android/app/src/main/kotlin/com/me/islami/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.me.islami
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MyHomeScreen extends StatelessWidget {
+  static const String route = 'home';
+
+  const MyHomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.black,
+            title: const Text('Islami')
+        ),
+        backgroundColor: Colors.grey,
+        body:
+    );
+  }
+}
