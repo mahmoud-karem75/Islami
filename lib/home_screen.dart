662f8859a4eb937c04260f0c76d30087c85d6731
mahmoud-karem75 <mmahmoudkarem7@gmail.com>
2022-08-18 13:10:13 +0200

    adding the bottom navigation bar

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -7,12 +7,44 @@ class MyHomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar:
-            AppBar(backgroundColor: Colors.black, title: const Text('Islami')),
-        backgroundColor: Colors.grey,
-        body: const Center(
-          child: Text('text'),
-        ));
+    return Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(
+                'assets/bg3.png',
+              ),
+              fit: BoxFit.fill)),
+      child: Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            title: const Center(
+              child: Text('اسلامي',
+                  style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w500,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.black)),
+            )),
+        bottomNavigationBar:
+            BottomNavigationBar(backgroundColor: Colors.brown, items: const [
+          BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage('assets/icons/radio.png'),
+                  color: Colors.white),
+              label: 'Radio',
+              tooltip: 'Radio'),
+          BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage('assets/3x/sebha.png')),
+              label: 'Sebha'),
+          BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage('assets/3x/hadeth.png')),
+              label: 'Hadith'),
+          BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage('assets/3x/quran.png')),
+              label: 'Quran'),
+        ]),
+        backgroundColor: Colors.transparent,
+      ),
+    );
   }
 }
